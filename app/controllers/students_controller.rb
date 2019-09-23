259d62b461f7e9b4e1160a80ab47134ee2528b5b
Kevin Karma <kevinkarma55@gmail.com>
2019-09-23 00:17:35 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -19,7 +19,7 @@ class StudentsController < ApplicationController
   private 
   
   def student_params
-    params.require(:student).permit(:first_name,:last_name)
+    params.require(:students).permit(:first_name,:last_name)
   end
 
 end
